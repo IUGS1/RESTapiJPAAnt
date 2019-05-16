45f9da988ad85ccea03faa2f32c6a3a38567ad88
Ivancito <ivan.guzman@towasoftware.com>
2019-05-16 12:56:14 -0500

    Added comments to DateProbeResource class

--- src/java/apirest/DateProbeResource.java
+++ src/java/apirest/DateProbeResource.java
@@ -1,4 +1,20 @@
+/*
+    Date
+    The class Date represents a specific instant in time, with millisecond precision.
 
+    Prior to JDK 1.1, the class Date had two additional functions. It allowed the interpretation of dates as year, 
+    month, day, hour, minute, and second values. It also allowed the formatting and parsing of date strings. 
+    Unfortunately, the API for these functions was not amenable to internationalization. As of JDK 1.1, the Calendar 
+    class should be used to convert between dates and time fields and the DateFormat class should be used to format and 
+    parse date strings. The corresponding methods in Date are deprecated.
+    Extracted from: https://docs.oracle.com/javase/8/docs/api/java/util/Date.html
+
+    SimpleDateFormat
+    SimpleDateFormat allows you to start by choosing any user-defined patterns for date-time formatting.
+    Extracted from: https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html
+
+
+*/
 package apirest;
 
 import javax.ws.rs.Path;
